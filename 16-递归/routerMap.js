686bdf0acead7281a42f806754519a35eafb7051
dongleicode <mhwgodl@163.com>
2023-01-03 09:31:10 +0800

    Reflect.get(target, key, receive)

---
+++ 16-递归/routerMap.js
@@ -0,0 +1,7 @@
+
+const routerMap = {
+    menu: () => import('@/views/system/menu/index'),
+    role: () => import('@/views/system/role/index'),
+    first: () => import('@/views/dashboard/index'),
+    layout: "Layout"
+}
